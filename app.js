44e657bbb3764274ec66ea574b8a4ff708d56ab5
Sarah Santiago <92765415+Sarah-M-S@users.noreply.github.com>
2023-06-04 02:02:02 -0300

    backend files

---
+++ app.js
@@ -0,0 +1,102 @@
+//importações de módulos
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const path = require("path");
+const Sequelize = require("sequelize");
+
+//conectando com o banco
+const sequelize = new Sequelize("skyifsp", "root", "1234", {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+sequelize.authenticate().then(function(){
+    console.log("deu certo!");
+}).catch(function(){
+    console.log("aAaAaAaAaAaAai")
+})
+
+const Voo = sequelize.define("Voo", {
+    ID_VOO: {
+        type: Sequelize.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    ORIGEM: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    DESTINO: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    DT_PARTIDA: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    DT_CHEGADA: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    HR_SAIDA: {
+        type: Sequelize.TIME,
+        allowNull: false,
+    },
+    HR_CHEGADA: {
+        type: Sequelize.TIME,
+        allowNull: false,
+    },
+    VO_AERONAVE: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
+
+}, {tableName: "voo", 
+    timestamps: false,
+})
+
+//sincronizando model com banco de dados
+Voo.sync().then(() => {
+    console.log('Tabela sincronizada com sucesso');
+  })
+  .catch((error) => {
+    console.error('Erro ao sincronizar tabela:', error);
+  });
+
+//buscando registros no banco
+Voo.findAll().then((voos) => {
+    app.post("/passagensAereas", function(req, res){
+        res.render("passagensAereas", {voos: voos});
+        //res.send("testando" + req.body.Origem + "\n" + req.body.Destino + "\n" + req.body.dataSaida);
+    })
+}).catch((error) => {
+    console.error('Erro ao consultar tabela:', error);
+  });
+
+
+
+//=========================================================================================
+
+
+//config
+    //template engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//rotas
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+
+
+
+
+app.listen(8083, function(){
+    console.log("aAaAaAaAai está funcionando mesmo")
+})
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 
-  // //importacoes de modulos
-  // const express = require('express');
-  // const mysql = require('mysql');
-  // const app = express();
-  // const port = 3000;
+  const express = require('express');
+    const mysql = require('mysql'); //importando módulos
+    const app = express();
+    const port = 3000;
 
-  // //dados para a conexao  
-  // const connection = mysql.createConnection({
-  //   host: 'localhost',
-  //   user: 'root',
-  //   password: '1234',
-  //   database: 'skyifsp'
-  // });
-
-  // // Conecta ao banco de dados verificando se há erro
-  // connection.connect((error) => {
-  //   if (error) {
-  //     console.error('Erro ao conectar ao banco de dados: ', error);
-  //   } 
-  //   else {
-  //     console.log('Conexão bem-sucedida ao banco de dados!');
-  //   }
-  //   });
+    
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'skyifsp'
+  });
 
 
-  // connection.query(`SELECT * FROM voo`, (error, results) => {
-  //   if (error) {
-  //     console.error('Erro ao consultar o banco de dados:', error);
-  //     return;
-  //   }
+  // Conecta-se ao banco de dados
+  connection.connect((error) => {
+      if (error) {
+        console.error('Erro ao conectar ao banco de dados: ', error);
+      } else {
+        console.log('Conexão bem-sucedida ao banco de dados!');
+      }
+    });
+
+
+    
+
+  connection.query(`SELECT * FROM voo`, (error, results) => {
+    if (error) {
+      console.error('Erro ao consultar o banco de dados:', error);
+      return;
+    }
   
-  //   if (results.length > 0) {
-  //     console.log('Registros do banco de dados:');
-  //     results.forEach((registro) => {
-  //       console.log(registro);
-  //     });
-  //   } else {
-  //     console.log('Nenhum registro encontrado.');
-  //   }
-  // });
+    if (results.length > 0) {
+      console.log('Registros do banco de dados:');
+      results.forEach((registro) => {
+        console.log(registro);
+      });
+    } else {
+      console.log('Nenhum registro encontrado.');
+    }
+  });
 
 
-  const conexao = require("./conector-banco");
-  conexao();
+  
